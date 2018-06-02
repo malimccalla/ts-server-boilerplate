65ded864683e8e97d924bd2269b4228756aafc53
Mali Michael <malimccalla@gmail.com>
2018-06-02 13:31:01 +0100

    Set up apollo server 2

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,23 @@
 import 'reflect-metadata';
-import { createConnection } from 'typeorm';
-import { User } from './entity/User';
+import { ApolloServer, gql } from 'apollo-server';
 
-createConnection()
-  .then(async connection => {
-    console.log('Inserting a new user into the database...');
-    const user = new User();
-    user.firstName = 'Timber';
-    user.lastName = 'Saw';
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log('Saved a new user with id: ' + user.id);
+const typeDefs = gql`
+  type Query {
+    hello(name: String): String!
+  }
+`;
 
-    console.log('Loading users from the database...');
-    const users = await connection.manager.find(User);
-    console.log('Loaded users: ', users);
+const resolvers = {
+  Query: {
+    hello: (_: any, { name }: any) => `Hello ${name || 'world'}`,
+  },
+};
 
-    console.log('Here you can setup and run express/koa/any other framework.');
-  })
-  .catch(error => console.log(error));
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`server listening at ${url}`);
+});
