139eb2e4cb502750636295b285d30331df6f24cf
Mali Michael <malimccalla@gmail.com>
2018-06-03 12:21:56 +0100

    Validate register args

--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -1,5 +1,19 @@
 import { User } from '../../entity/User';
 import { ResolverMap } from '../../types';
+import * as yup from 'yup';
+import { formatYupError } from '../../util/formatYupError';
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .min(3)
+    .max(255)
+    .email(),
+  password: yup
+    .string()
+    .min(8, 'Password must be longer than 8 characters')
+    .max(255),
+});
 
 const resolvers: ResolverMap = {
   Mutation: {
@@ -7,9 +21,20 @@ const resolvers: ResolverMap = {
       _: any,
       args: GQL.IRegisterOnMutationArguments
     ): Promise<GQL.IRegisterResponse> => {
+      try {
+        await schema.validate(args, { abortEarly: false });
+      } catch (error) {
+        return {
+          ok: false,
+          user: null,
+          errors: formatYupError(error),
+        };
+      }
+
+      const { email } = args;
       const userAlreadyExists = await User.findOne({
         select: ['id'],
-        where: { email: args.email },
+        where: { email },
       });
 
       if (userAlreadyExists) {
