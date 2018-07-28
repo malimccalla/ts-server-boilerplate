5924c3d893e84a753704ee2c09b54799451829dd
Mali Michael <malimccalla@gmail.com>
2018-07-28 14:25:51 +0100

    Add login resolver

--- src/modules/login/resolvers.ts
+++ src/modules/login/resolvers.ts
@@ -1,9 +1,40 @@
+import * as bcrypt from 'bcryptjs';
+
+import { User } from '../../entity/User';
 import { ResolverMap } from '../../types';
 
 const resolvers: ResolverMap = {
   Mutation: {
-    login: async (_: any): Promise<any> => {
-      return 'ok';
+    login: async (
+      _: any,
+      { email, password }: GQL.ILoginOnMutationArguments
+    ): Promise<GQL.ILoginResponse> => {
+      const errorResponse: GQL.ILoginResponse = {
+        ok: false,
+        errors: [{ path: 'login', message: 'Invalid credentials' }],
+        user: null,
+      };
+
+      const user = await User.findOne({ where: { email } });
+      if (!user) return errorResponse;
+
+      const valid = await bcrypt.compare(password, user.password);
+      if (!valid) return errorResponse;
+
+      if (!user.confirmed) {
+        return {
+          ok: false,
+          errors: [{ message: 'Please confirm your email address', path: 'email' }],
+          user: null,
+        };
+      }
+
+      // exist in database and have given correct credentials
+      return {
+        ok: true,
+        errors: [],
+        user,
+      };
     },
   },
 };
